881f27e6101b6f776e2cbc038cb1267c5207dffb
jeffwoolums <jeffwoolums@mac.com>
2025-07-14 13:09:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -46,3 +46,7 @@ def scrape():
 
 if __name__ == '__main__':
     app.run()
+    import os
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
